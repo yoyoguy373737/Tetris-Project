5309a53a7cce92cf4ba3dc09a4ebee960ee803cb
micheung7 <783230@CA-iMacF11-15.local>
2016-05-17 10:13:54 -0700

    Square code

--- Square.java
+++ Square.java
@@ -1,4 +1,18 @@
+package Tetris;
 
-public class Square {
+import java.awt.Rectangle;
 
+public class Square extends Rectangle {
+	private int length;
+
+	public Square(int x, int y, int length) {
+		super(x, y);
+		this.length = length;
+	}
+
+	public boolean overlap(Block b) {
+		boolean c = (getX() <= b.getX() + length) && (getX() >= b.getX());
+		boolean d = (y <= b.getY() + length) && (y >= b.getY());
+		return c && d;
+	}
 }
